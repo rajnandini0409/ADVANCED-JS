7cacd51ea2497ed67ca327ecd56c1019f5d40971
rajnandini0409 <rajnandinisoni04@gmail.com>
2025-04-08 13:07:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+// function sayHello(){
+//     let you = prompt("What is your name");
+//     console.log("Hello"+you+ "!");
+//     }
+// sayHello();
+
+// let varContainingFunction = function(){
+//     let varInFunction="I'm in a function";
+//     console.log("hi there!",varInFunction);
+// };
+// varContainingFunction();
+// function tester(para1,para2){
+//     return para1+""+para2;
+
+// }
+// const agr1 = "argument 1";
+// const agr2 = "argument 2";
+//  console.log (tester(agr1,agr2));
+
+// let x1=parseInt(prompt("enter the first number"));
+// let x2 = parseInt(prompt("enter the ssecond number"));
+// addTwoNumbers(x1,x2);
+// function addTwoNumbers(x,y){
+//     console.log(x+y);
+// }
+// function addTwoNumbers(x=2,y=3){
+//     console.log(x+y);
+// }
+// addTwoNumbers(4,5);//arrow function are great for sending function around parameters using shorter notation
+
+// represented by =>
+// let doingArrowStuff=  x => console.log(x);
+// doingArrowStuff("great!");
+
+// let addTwoNumbers = (x,y)=> console.log(x+y);
+// addTwoNumbers(2,4);
+
+// const arr=["squirrel","alpac","buddy"];
+// arr.forEach(e => console.log(e));
+  
+// SPREAD OPERATOR
+
+// let spread = ["so","much","fun"];
+// let message =["javascript","is",...spread,"and","very","powerful"
+// ];
+// console.log(message);
+
+// function addTwoNumbers(x,y){
+//        console.log(x+y);
+
+// }
+// let arr =[5,9];
+// addTwoNumbers(...arr);
+// function addTwoNumbers(x,y,z,a){
+//     console.log(x+y+z+a);
+
+// }
+// let arr =[5,9];
+// let arr2 = [6,7];
+// addTwoNumbers(...arr ,...arr2);
+
+
+// REST PARAMETER 
+
+// function someFunction(p1,...p2){
+//     console.log(p1,p2);
+// }
+// someFunction("hi","there!","how are you");
+
+// RETURNING FUNCTION VALUES
+
+// let result =addTwoNumbers(4,5);
+// console.log(result);
+
+// let addTwoNumbers=(x,y) => x+y;
+
+// let resultarr=[];
+// for(let i=0;i<10;i++){
+//     let result=addTwoNumbers(i,2*i);
+//     resultarr.push(result);
+// }
+// console.log(resultarr);
+
+
+// RETURNING WITH ARROW FUNCTION
+
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let x1=parseInt(prompt("enter the first number"));
+let x2 = parseInt(prompt("enter the ssecond number"));
+addTwoNumbers(x1,x2);
+function addTwoNumbers(x,y){
+    console.log(x+y);
+}
